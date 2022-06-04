b5b74da84e721a696ca05be7f888a5a6b0d8d403
Aimi Fadhil <59568628+ColonelBucket8@users.noreply.github.com>
2022-06-04 22:47:26 +0800

    added cart context and function to toggle cart dropdown

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,9 +1,16 @@
+import { useContext } from "react";
+import { CartContext } from "../../contexts/cart.context";
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 import "./cart-icon.style.scss";
 
 const CartIcon = () => {
+  const { showCart, setShowCart } = useContext(CartContext);
+
+  const toggleShowCart = () => {
+    setShowCart(!showCart);
+  };
   return (
-    <div className="cart-icon-container">
+    <div className="cart-icon-container" onClick={toggleShowCart}>
       <ShoppingIcon className="shopping-icon" />
       <span className="item-count">0</span>
     </div>
