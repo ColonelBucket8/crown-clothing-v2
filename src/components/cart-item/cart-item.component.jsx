474ab448880def26ddc7e777fd68b495a1c0c2ed
ColonelBucket8 <59568628+ColonelBucket8@users.noreply.github.com>
2023-01-09 22:48:30 +0800

    Make styled components for cart item component

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,18 +1,24 @@
-import "./cart-item.style.scss";
+import './cart-item.style';
+import {
+  CartItemContainer,
+  ImageContainer,
+  ItemDetails,
+  Name,
+} from './cart-item.style';
 
 const CartItem = ({ cartItem }) => {
   const { name, imageUrl, quantity, price } = cartItem;
 
   return (
-    <div className="cart-item-container">
-      <img src={imageUrl} alt={`${name}`} />
-      <div className="item-details">
-        <span className="name">{name}</span>
+    <CartItemContainer>
+      <img style={{ width: '30%' }} src={imageUrl} alt={`${name}`} />
+      <ItemDetails>
+        <Name>{name}</Name>
         <span className="price">
           {quantity} X ${price}
         </span>
-      </div>
-    </div>
+      </ItemDetails>
+    </CartItemContainer>
   );
 };
 
