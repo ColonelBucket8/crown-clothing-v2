71ea87a98242cec4e08b7602221aa039c14eeb1d
ColonelBucket8 <59568628+ColonelBucket8@users.noreply.github.com>
2023-01-20 23:43:19 +0800

    Create user selector and pick current user at navigation

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,7 +1,7 @@
 import { Fragment, useContext } from 'react';
+import { useSelector } from 'react-redux';
 import { Outlet } from 'react-router-dom';
 import { ReactComponent as CrownLogo } from '../../assets/crown.svg';
-import { UserContext } from '../../contexts/user.context';
 import { CartContext } from '../../contexts/cart.context';
 import { signOutUser } from '../../utils/firebase/firebase.utils';
 import CartIcon from '../../components/cart-icon/cart-icon.component';
@@ -13,9 +13,10 @@ import {
   NavLink,
   LogoContainer,
 } from './navigation.style';
+import { selectCurrentUser } from '../../store/user/user.selector';
 
 const Navigation = () => {
-  const { currentUser } = useContext(UserContext);
+  const currentUser = useSelector(selectCurrentUser);
   const { showCart } = useContext(CartContext);
 
   return (
