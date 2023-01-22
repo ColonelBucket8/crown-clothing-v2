fd2fabc659d7acc748b1246fbd5f0e2ab3e4c328
ColonelBucket8 <59568628+ColonelBucket8@users.noreply.github.com>
2023-01-22 13:43:19 +0800

    Add redux persist 1. Persist categories and cart items

--- src/index.js
+++ src/index.js
@@ -4,17 +4,20 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import './index.scss';
 import App from './App';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import reportWebVitals from './reportWebVitals';
-import { store } from './store/store';
+import { persistor, store } from './store/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
