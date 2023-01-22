f61412f145216f6d75f95e4fa3297d518b8b4554
ColonelBucket8 <59568628+ColonelBucket8@users.noreply.github.com>
2023-01-22 12:26:20 +0800

    Install reselect 1. Memoization of selectors function 2. Memoize categories selectors

--- src/store/categories/categories.selector.js
+++ src/store/categories/categories.selector.js
@@ -1,7 +1,19 @@
-export const selectCategoriesMap = (state) => {
-  return state.categories.categories.reduce((acc, category) => {
-    const { title, items } = category;
-    acc[title.toLowerCase()] = items;
-    return acc;
-  }, {});
-};
+import { createSelector } from 'reselect';
+
+const selectCategoriesReducer = (state) => state.categories;
+
+export const selectCategories = createSelector(
+  [selectCategoriesReducer],
+  (categoriesSlice) => categoriesSlice.categories
+);
+
+export const selectCategoriesMap = createSelector(
+  [selectCategories],
+  (categories) => {
+    return categories.reduce((acc, category) => {
+      const { title, items } = category;
+      acc[title.toLowerCase()] = items;
+      return acc;
+    }, {});
+  }
+);
