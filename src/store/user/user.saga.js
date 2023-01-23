d64ed11071413d6bf3e2c94838c9933ea6234c60
ColonelBucket8 <59568628+ColonelBucket8@users.noreply.github.com>
2023-01-24 07:13:36 +0800

    Change user to user saga

---
+++ src/store/user/user.saga.js
@@ -0,0 +1,39 @@
+import { takeLatest, put, all, call } from 'redux-saga/effects';
+import { USER_ACTION_TYPES } from './user.types';
+import { signInSuccess, signInFailed } from './user.action';
+import {
+  getCurrentUser,
+  createUserDocumentFromAuth,
+} from '../../utils/firebase/firebase.utils';
+
+export function* getSnapshotFromUserAuth(userAuth, additionalDetails) {
+  try {
+    const userSnapshot = yield call(
+      createUserDocumentFromAuth,
+      userAuth,
+      additionalDetails
+    );
+    yield put(signInSuccess({ id: userSnapshot.id, ...userSnapshot.data() }));
+  } catch (e) {
+    yield put(signInFailed(e));
+  }
+}
+
+export function* isUserAuthenticated() {
+  try {
+    const userAuth = yield call(getCurrentUser);
+    if (!userAuth) return;
+
+    yield call(getSnapshotFromUserAuth, userAuth);
+  } catch (e) {
+    yield put(signInFailed(e));
+  }
+}
+
+export function* onCheckUserSession() {
+  yield takeLatest(USER_ACTION_TYPES.CHECK_USER_SESSION, isUserAuthenticated);
+}
+
+export function* userSaga() {
+  yield all([onCheckUserSession]);
+}
