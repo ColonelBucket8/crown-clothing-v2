5f33199c3a04eba564e9082c04a4a09dcf997458
ColonelBucket8 <59568628+ColonelBucket8@users.noreply.github.com>
2023-01-24 18:21:19 +0800

    Create stripe payment form component

---
+++ src/components/payment-form/payment-form.component.jsx
@@ -0,0 +1,30 @@
+import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
+
+import Button, { BUTTON_TYPE_CLASSES } from '../button/button.component';
+
+import { PaymentFormContainer, FormContainer } from './payment-form.style';
+
+const PaymentForm = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const paymentHandler = async (evt) => {
+    evt.preventDefault();
+
+    if (!stripe || !elements) {
+      return;
+    }
+  };
+
+  return (
+    <PaymentFormContainer>
+      <FormContainer>
+        <h2>Credit Card Payment: </h2>
+        <CardElement />
+        <Button buttonType={BUTTON_TYPE_CLASSES.inverted}>Pay now</Button>
+      </FormContainer>
+    </PaymentFormContainer>
+  );
+};
+
+export default PaymentForm;
