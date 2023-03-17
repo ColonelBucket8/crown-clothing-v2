9b3a8b7d3d9bc24265a5c7c02b2e58d9cfff6294
ColonelBucket8 <59568628+ColonelBucket8@users.noreply.github.com>
2023-03-17 22:38:34 +0800

    Add test for category preview component

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
+import { MemoryRouter } from 'react-router-dom';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  render(
+    <MemoryRouter initialEntries={['/']}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </MemoryRouter>
+  );
+
+  expect(true).toBeTruthy();
 });
---
+++ src/components/category-preview/category-preview.component.test.tsx
@@ -0,0 +1,46 @@
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import CategoryPreview from './category-preview.component';
+import { Provider } from 'react-redux';
+import { store } from '../../store/store';
+import '@testing-library/jest-dom';
+import { MemoryRouter } from 'react-router-dom';
+
+const categoryItems = [
+  {
+    id: 1,
+    imageUrl: 'https://example.com/item1.jpg',
+    name: 'Item 1',
+    price: 10.99,
+  },
+  {
+    id: 2,
+    imageUrl: 'https://example.com/item2.jpg',
+    name: 'Item 2',
+    price: 19.99,
+  },
+  {
+    id: 3,
+    imageUrl: 'https://example.com/item3.jpg',
+    name: 'Item 3',
+    price: 5.99,
+  },
+];
+
+describe('Category Preview component', () => {
+  it('should render the category preview component', () => {
+    render(
+      <Provider store={store}>
+        <MemoryRouter initialEntries={['/shop']}>
+          <CategoryPreview title={'example'} products={categoryItems} />
+        </MemoryRouter>
+      </Provider>
+    );
+
+    expect(screen.getByText(/example/i)).toBeInTheDocument();
+    categoryItems.forEach((item) => {
+      expect(screen.getByText(item.name)).toBeInTheDocument();
+      expect(screen.getByText(item.price)).toBeInTheDocument();
+    });
+  });
+});
